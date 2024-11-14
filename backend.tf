d9cfe1b701e2c634688f9fe0cbce44950d0a3048
Collins Orighose <collinsefe@yahoo.com>
2024-11-14 18:29:36 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket         = "collins-capgem-demo-05112024"
     key            = "capgem-app/infra.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "CollinsDemo"
-    encrypt        = false
+    // dynamodb_table = "CollinsDemo"
+    // encrypt        = false
   }
 
 }
